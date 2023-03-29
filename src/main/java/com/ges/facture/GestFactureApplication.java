6535fdbe581825e533ce5732dbb5e8fac711efc7
EMEAAD\zothmani <zied.othmani@soprasteria.com>
2023-03-29 12:37:19 +0200

    project initial gestion des facturation

---
+++ src/main/java/com/ges/facture/GestFactureApplication.java
@@ -0,0 +1,13 @@
+package com.ges.facture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestFactureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestFactureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ges/facture/GestFactureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ges.facture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestFactureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
